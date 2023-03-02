eebceddcfe8582c620b87151c4fafc053c7d9570
BjornSevenhuijsenPXL <12000633@student.pxl.be>
2023-03-02 09:11:28 +0100

    Updated framework

--- src/main/java/wisemen/testObjects/utils/ObjectStrategy.java
+++ src/main/java/wisemen/testObjects/utils/ObjectStrategy.java
@@ -2,17 +2,15 @@ package wisemen.testObjects.utils;
 
 import wisemen.mobile.screenObjects.AllScreens;
 import wisemen.testObjects.objects.IAllObjects;
-import wisemen.web.pageobjects.AllPages;
 
 public class ObjectStrategy {
-    public static IAllObjects createTestObject(){
+    public static IAllObjects createTestObject() {
         return createTestObject(ObjectConfig.getTestObject());
     }
 
-    public static IAllObjects createTestObject(String testObject){
+    public static IAllObjects createTestObject(String testObject) {
 
         return switch (testObject) {
-            case "web" -> new AllPages();
             case "mobile" -> new AllScreens();
             default -> null;
         };
