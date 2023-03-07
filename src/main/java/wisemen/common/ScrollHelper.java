da7923d24b86db9be39289075e1b159ce7b87c5b
BjornSevenhuijsenPXL <12000633@student.pxl.be>
2023-03-07 14:18:24 +0100

    Create ScrollHelper.java

---
+++ src/main/java/wisemen/common/ScrollHelper.java
@@ -0,0 +1,11 @@
+package wisemen.common;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+
+public class ScrollHelper {
+    /*public static WebElement scrollFindElementContainsText(Driver driver, String textToFind){
+        return driver.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true))" +
+                ".scrollIntoView(new UiSelector().textContains(\"" + "already have an account" + "\").instance(0))"));
+    }*/
+}
